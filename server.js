211459f11200d90a4195a9b6ebaf14c62fa512cb
Isaiah Abiodun <isaiah.abiodun@outlook.com>
2021-07-05 17:22:52 +0000

    server tut

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
 const http = require('http');
 
 // Create a server with HTTP Variable
-http.createServer;
+const server = http.createServer((req, res) => {
+    // Headers
+    // res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    // Send back some information
+    // res.end('Welcome to First Node Server');
+
+    res.end(`
+        {
+            "name":  "Abisalde",
+            "location": "Lagos",
+            "Occupation": "Software Developer"
+        }
+    `);
+});
+
+// Create a port
+server.listen(5600, '127.0.0.1');
+console.log('server <------> started');
